4091296c430ab12dd54d88252c256d243dea568f
HuangStomach <nxmbest@qq.com>
2021-05-20 13:48:05 +0800

    vaen 数据筛选代码迁移

--- 20210519-vaen/Data/prepare_ZS.py
+++ 20210519-vaen/Data/prepare_ZS.py
@@ -6,4 +6,8 @@ ccle = pandas.read_table("./CCLE/CCLE_DepMap_18q3_RNAseq_RPKM_20180718.gct", hea
 tcga_rpkm = pandas.read_table("./TCGA/ACC/HiSeqV2", header = 0).to_numpy() # 读取肿瘤基因组图谱中对应肿瘤的测序结果
 
 genes = np.intersect1d(ccle[:, 1], tcga_rpkm[:, 0]) # 获取有交集的基因 len(genes) 18217
-new_ccle = np.array(list(filter(lambda cell: cell in genes, ccle))) # 过滤包含在交集基因中的数据
+new_ccle = ccle[np.in1d(ccle[:, 1], genes)] # 过滤包含在交集基因中的数据
+# 排除表达度低的基因, avg.RPKM > 1 in CCLE shape (12437, 1156)
+ccle_gene_mat = np.array(list(filter(lambda cell_line: np.mean(cell_line) > 1, new_ccle[:, 2:]))) 
+
+# 选择有>=20个细胞系的组织
