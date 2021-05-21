ffce6f369c0d3d842ccf0369b0e5602ce543a732
HuangStomach <nxmbest@qq.com>
2021-05-21 15:00:29 +0800

    vaen 按照TCGA目录下的癌症类型进行数据整理

--- 20210519-vaen/Data/prepare_ZS.py
+++ 20210519-vaen/Data/prepare_ZS.py
@@ -1,3 +1,4 @@
+import os
 import pandas
 import numpy as np
 np.set_printoptions(threshold=np.inf)
@@ -9,7 +10,9 @@ tcga_rpkm = pandas.read_table("./TCGA/ACC/HiSeqV2", header = 0).to_numpy() # 读
 genes = np.intersect1d(ccle[:, 1], tcga_rpkm[:, 0]) # 获取有交集的基因 len(genes) 18217
 new_ccle = ccle[np.in1d(ccle[:, 1], genes)] # 过滤包含在交集基因中的数据
 # 排除表达度低的基因, avg.RPKM > 1 in CCLE shape (12437, 1156)
-ccle_gene_mat = np.array(list(filter(lambda cell_line: np.mean(cell_line) > 1, new_ccle[:, 2:]))).astype(np.float64)
+ccle_gene_mat = np.array(list(filter(lambda cell_line: np.mean(cell_line[2:]) > 1, new_ccle)))
+ccle_gene_mat_names = ccle_gene_mat[:, :2] # 分离符合条件的基因名称
+ccle_gene_mat = ccle_gene_mat[:, 2:].astype(np.float64) # 分离符合条件的基因数据
 
 # 选择有 >=20 个细胞系的组织
 columns = []
@@ -31,7 +34,23 @@ log2_ccle_gene_mat = np.log2(ccle_gene_mat + 1)
 variance = log2_ccle_gene_mat.var(1)
 mid = np.median(variance)
 mad5000_ccle_gene_mat = []
-for cell in log2_ccle_gene_mat:
-    if cell.var() > mid: mad5000_ccle_gene_mat.append(cell)
+mad_genes = []
+for i, cell in enumerate(log2_ccle_gene_mat):
+    if cell.var() > mid: 
+        # 同时筛选数据和基因名称
+        mad5000_ccle_gene_mat.append(cell)
+        mad_genes.append(ccle_gene_mat_names[i])
+
 mad5000_ccle_gene_mat = np.array(mad5000_ccle_gene_mat) # shape (6219, 1100)
-print(mad5000_ccle_gene_mat.shape)
+mad_genes = np.array(mad_genes)
+
+# 按照TCGA目录下的癌症类型进行数据整理
+cancer_types = os.listdir('./TCGA')
+cancer_types.sort()
+
+for i, cancer in enumerate(cancer_types):
+    tcga_rpkm = pandas.read_table("./TCGA/{}/HiSeqV2".format(cancer), header = 0).to_numpy()
+    # 排除行和为0的基因
+    tcga_rpkm = np.array([row for row in tcga_rpkm if sum(row) != 0])
+    tcga_rpkm = tcga_rpkm[np.in1d(mad_genes[:, 1], tcga_rpkm[:, 0])] # 筛选基因有交集的数据
+    tcga_rpkm = np.array([row for row in tcga_rpkm if sum(row) != np.NaN]) # 祛除错误数据
