f6998989967ba351a428ee323f623af714ec6c1b
HuangStomach <nxmbest@qq.com>
2021-05-21 15:44:01 +0800

    vaen 修正数据合并

--- 20210519-vaen/Data/prepare_ZS.py
+++ 20210519-vaen/Data/prepare_ZS.py
@@ -1,7 +1,7 @@
 import os
 import pandas
 import numpy as np
-np.set_printoptions(threshold=np.inf)
+#np.set_printoptions(threshold=np.inf)
 
 ccle_table = pandas.read_table("./CCLE/CCLE_DepMap_18q3_RNAseq_RPKM_20180718.gct", header = 0, skiprows = 2) # 读取对应的肿瘤细胞系
 ccle = ccle_table.to_numpy()
@@ -48,9 +48,51 @@ mad_genes = np.array(mad_genes)
 cancer_types = os.listdir('./TCGA')
 cancer_types.sort()
 
+rpkm_mat = np.array([])
+cur_genes = []
 for i, cancer in enumerate(cancer_types):
     tcga_rpkm = pandas.read_table("./TCGA/{}/HiSeqV2".format(cancer), header = 0).to_numpy()
-    # 排除行和为0的基因
-    tcga_rpkm = np.array([row for row in tcga_rpkm if sum(row) != 0])
-    tcga_rpkm = tcga_rpkm[np.in1d(mad_genes[:, 1], tcga_rpkm[:, 0])] # 筛选基因有交集的数据
-    tcga_rpkm = np.array([row for row in tcga_rpkm if sum(row) != np.NaN]) # 祛除错误数据
+    # 排除行和为0和错误的数据的基因
+    tcga_rpkm = np.array([row for row in tcga_rpkm if sum(row[1:]) != np.NaN or sum(row[1:]) != 0])
+    tcga_rpkm = tcga_rpkm[np.in1d(tcga_rpkm[:, 0], mad_genes[:, 1])] # 筛选基因有交集的数据
+
+    if i == 0: cur_genes = tcga_rpkm[:, 0]
+    else: cur_genes = np.intersect1d(cur_genes, tcga_rpkm[:, 0]) # 不停获取基因的交集
+
+    if len(rpkm_mat) == 0: rpkm_mat = tcga_rpkm[np.in1d(tcga_rpkm[:, 0], cur_genes)]
+    else: rpkm_mat = rpkm_mat[np.in1d(rpkm_mat[:, 0], cur_genes)] + tcga_rpkm[np.in1d(tcga_rpkm[:, 0], cur_genes)]
+    print(cancer, "\t", len(tcga_rpkm[0]), len(rpkm_mat), len(rpkm_mat[0]))
+
+#ACC     79 6203 79
+#BLCA    426 6203 505
+#BRCA    1218 6203 1723
+#CESC    308 6203 2031
+#CHOL    45 6203 2076
+#COAD    329 6203 2405
+#DLBC    48 6203 2453
+#ESCA    196 6203 2649
+#GBM     172 6203 2821
+#HNSC    566 6203 3387
+#KICH    91 6203 3478
+#KIRC    606 6203 4084
+#KIRP    323 6203 4407
+#LAML    173 6203 4580
+#LGG     530 6203 5110
+#LIHC    423 6203 5533
+#LUAD    576 6203 6109
+#LUSC    553 6203 6662
+#MESO    87 6203 6749
+#OV      308 6203 7057
+#PAAD    183 6203 7240
+#PCPG    187 6203 7427
+#PRAD    550 6203 7977
+#READ    105 6203 8082
+#SARC    265 6203 8347
+#SKCM    474 6203 8821
+#STAD    450 6203 9271
+#TGCT    156 6203 9427
+#THCA    572 6203 9999
+#THYM    122 6203 10121
+#UCEC    201 6203 10322
+#UCS     57 6203 10379
+#UVM     80 6203 10459
