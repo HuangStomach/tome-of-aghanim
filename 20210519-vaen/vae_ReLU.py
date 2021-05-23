0d84089368addc97d5962e9e54bf4fb8ae39bd1b
HuangStomach <nxmbest@qq.com>
2021-05-23 15:36:33 +0800

    调整初始化代码兼容性

--- 20210519-vaen/vae_ReLU.py
+++ 20210519-vaen/vae_ReLU.py
@@ -4,8 +4,41 @@ import random as rn
 import numpy as np
 import pandas as pd
 
-from tensorflow import keras
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import backend as K
 
-work_dir = sys.argv[2]
-print(sys.argv)
-print("work_dir: %s", work_dir)
+# 固定种子，创造可复现结果
+os.environ['PYTHONHASHSEED'] = 0
+session_conf = tf.compat.v1.ConfigProto(
+    intra_op_parallelism_threads=1,
+    inter_op_parallelism_threads=1
+)
+sess = tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph() ,config=session_conf)
+tf.compat.v1.keras.backend.set_session(session_conf)
+
+from tensorflow.keras import metrics, optimizers
+from tensorflow.keras.layers import Input, Dense, Lambda, Layer, Activation, Dropout, BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import Callback
+
+output_dir = './Output'
+train_file_path = './Data/V15.CCLE.4VAE.ZS.tsv'
+val_file_1_path = './Data/V15.TCGA.4VAE.ZS.tsv'
+train_latent_file = 'CCLE_latent.tsv'
+train_weight_file = 'CCLE_weight.tsv'
+
+predict_file = 'PANCAN_prediction.tsv'
+encoder_file = 'CCLE_encoder_onehidden_vae.hdf5'
+decoder_file = 'CCLE_decoder_onehidden_vae.hdf5'
+print("output_dir: {}".output_dir)
+
+epsilon_std = 1.0
+
+def sampling(args):
+    #import tensorflow as tf
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=tf.shape(input=z_mean), mean=0.,stddev=epsilon_std)
+    z = z_mean + K.exp(z_log_var / 2) * epsilon
+    return z
