e812cd1f0754490b818b209fa2eb6aa4e978d9de
HuangStomach <nxmbest@qq.com>
2021-06-04 11:57:34 +0800

    vaen 增加生成tcga_ss_mat脚本

---
+++ 20210519-vaen/1_TCGA_ss_mat.R
@@ -0,0 +1,18 @@
+## 补充脚本 生成正确的 TCGA.ss.mat.RData
+cancer_types <- dir("./Data/TCGA/")
+sapply(cancer_types, nchar) -> ii
+cancer_types <- cancer_types[which(ii <= 4)]
+cancer_types <- setdiff(cancer_types, c("FPPP", "LUNG"))
+
+tcga_ss_mat <- matrix(ncol = 2, nrow = 0)
+for (cancer in cancer_types) {
+    print(cancer)
+    tcga_rpkm <- read.delim(
+        paste("./Data/TCGA/", cancer, "/HiSeqV2", sep = ""),
+    as.is = T)
+    for (rpkm in colnames(tcga_rpkm)[-1]) {
+        tcga_ss_mat <- rbind(tcga_ss_mat, c(rpkm, cancer))
+    }
+}
+
+save(tcga_ss_mat, file = "./Output/1/TCGA_ss_mat.RData")
