e5ce548de16be5e1e8f56f05f11ebf9ef1edf18b
HuangStomach <nxmbest@qq.com>
2021-07-26 16:46:02 +0800

    aen 神经网络学习模式

--- 20210710-aen/2_ccle_nn.py
+++ 20210710-aen/2_ccle_nn.py
@@ -2,8 +2,12 @@ import joblib
 import pandas as pd
 import numpy as np
 np.set_printoptions(suppress=True)
-from sklearn.linear_model import ElasticNetCV
 import scipy.stats as st
+
+import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Dense, InputLayer
+from tensorflow.keras import Sequential
+from sklearn.metrics import r2_score
 
 # ccle_to_tissue = np.load('./Output/1/ccle_to_tissue.npy', allow_pickle=True).item()
 # tissues = np.unique(list(ccle_to_tissue.values()))
@@ -15,13 +19,21 @@ def normal_name(name):
         name = name[1:]
     return name
 rownames = np.array(list(map(normal_name, ccle_latent.index.values)))
-cell_names = rownames[np.char.find(rownames, 'HAEMATOPOIETIC') == -1] # 对细胞系名称进行格式化 去除造血细胞
 
 coach = pd.read_csv('./Data/CCLE/CCLE_NP24.2009_Drug_data_2015.02.24.csv')
 drugs = pd.unique(coach['Compound'])
 drugs.sort()
-intersect = np.intersect1d(cell_names, coach['CCLE Cell Line Name']) # 取细胞系交集
+intersect = np.intersect1d(rownames, coach['CCLE Cell Line Name']) # 取细胞系交集
 coach = coach.loc[coach['CCLE Cell Line Name'].isin(intersect)]
+
+def r2(y_true, y_pred):
+    a = K.square(y_pred - y_true)
+    b = K.sum(a)
+    c = K.mean(y_true)
+    d = K.square(y_true - c)
+    e = K.sum(d)
+    f = 1 - b/e
+    return f
 
 end = 2
 
@@ -41,31 +53,25 @@ for step in range(1, end + 1):
         for cell_name in coach_drug['CCLE Cell Line Name'].to_numpy(dtype=str):
             indexes.append(np.argwhere(rownames == cell_name)[0][0])
         
-        ccle_latent_drug = ccle_latent.iloc[indexes].to_numpy(dtype=np.float)
+        x = ccle_latent.iloc[indexes].to_numpy(dtype=np.float)
         y = coach_drug['ActArea'].to_numpy(dtype=np.float)
 
-        regr = ElasticNetCV(cv=10, max_iter=100000, random_state=0)
-        regr.fit(ccle_latent_drug, y)
-        pred = regr.predict(ccle_latent_drug)
-        score = regr.score(ccle_latent_drug, y)
-        print(drug, score)
-        
-        if drug not in models_info.keys() or score > models_info[drug]['r2_score']:
-            full_y = np.full(ccle_latent.shape[0], -9, dtype=float)
-            full_pred = np.full(ccle_latent.shape[0], -9, dtype=float)
-            full_y[indexes] = y
-            full_pred[indexes] = pred
+        model = Sequential()
+        model.add(InputLayer((x.shape[1], )))
+        model.add(Dense(100, activation='relu'))
+        model.add(Dense(1))
+        model.compile(loss='mse', optimizer='adam', metrics=[r2])
+        model.fit(x, y, epochs=100, validation_split=0.2, verbose=0)
 
+        pred = model.predict(x, verbose=1)[:, 0]
+        score = r2_score(y, pred)
+        print(drug, score)
+
+        if drug not in models_info.keys() or score > models_info[drug]['r2_score']:
             models_info[drug] = {
-                'y': full_y,
-                'pred': full_pred,
-                'step': step,
-                'size': len(y),
                 'pcc': st.pearsonr(y, pred),
                 'r2_score': score,
             }
-            models[drug] = regr
-            continue
 
-joblib.dump(models_info, './Output/2/ccle_s_models_info.joblib')  
-joblib.dump(models, './Output/2/ccle_s_models.joblib')  
+joblib.dump(models_info, './Output/2/ccle_nn_models_info.joblib')  
+# joblib.dump(models, './Output/2/ccle_models.joblib')  
