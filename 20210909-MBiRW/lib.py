2282ec46cef62a08be64c4ae6820e9ab1ece8d01
HuangStomach <nxmbest@qq.com>
2021-09-09 22:02:38 +0800

    mbirw set_par_fun

--- 20210909-MBiRW/lib.py
+++ 20210909-MBiRW/lib.py
@@ -1,8 +1,76 @@
 import numpy as np
+import random
+import math
 
+'''
+ass_mat 药物和疾病的关联矩阵
+sim_mat 药物或者疾病的相似矩阵
+'''
 def set_par_fun(ass_mat, sim_mat):
     num = sim_mat.shape[0]
     s_mat = np.zeros(10)
-    print(s_mat)
-    
 
+    s = np.zeros(10)
+    n = np.zeros(10)
+
+    for i in range(1, num):
+        for j in range(i + 1):
+            value = int(sim_mat[i][j] * 10)
+            sharedValue = np.dot(ass_mat[i], ass_mat[j])
+            if value > 9: value = 9
+
+            s[value] += 1
+            if sharedValue >= 1: n[value] += 1
+
+    for i in range(10):
+        s_mat[i] = s[i] / n[i]
+    
+    result_mat = np.zeros((10, 10))
+    for _i in range(10):
+        # 使用洗牌法进行打乱
+        s = np.zeros(10)
+        n = np.zeros(10)
+
+        m = 0
+        rsum = num * (num - 1) / 2 # 所有sim_mat配对可能的数量
+        dmat = np.zeros((1, rsum))
+
+        # 打乱之后放回原处
+        for i in range(1, num):
+            for j in range(i + 1):
+                dmat[m] = sim_mat[i][j]
+                m += 1
+        
+        for i in range(rsum):
+            j = random.randrange(i, rsum)
+            dmat[i], dmat[j] = dmat[j], dmat[i]
+        
+        m = 0
+        for i in range(1, num):
+            for j in range(i + 1):
+                sim_mat[i][j] = dmat[m];
+                m += 1
+
+        for i in range(1, num):
+            for j in range(i + 1):
+                value = int(sim_mat[i][j] * 10)
+                sharedValue = np.dot(ass_mat[i], ass_mat[j])
+                if value > 9: value = 9
+
+                s[value] += 1
+                if sharedValue >= 1: n[value] += 1
+
+    for i in range(10):
+        result_mat[_i][i] = s[i] / n[i]
+    r_mat = np.mean(result_mat, axis=1)
+    
+    threshold = 0;
+    for i in range(10):
+        result_p = s_mat[i]
+        result_pr = r_mat[i]
+
+        if result_p >= 1 * result_pr:
+            threshold = i * 0.1;
+            break
+
+    return (math.log(99) - math.log(9999)) / threshold
--- 20210909-MBiRW/main.py
+++ 20210909-MBiRW/main.py
@@ -4,13 +4,14 @@ import numpy as np
 from scipy.io import loadmat 
 import lib
 
-drugs_sim = pd.read_table('./Data/DrugSimMat', sep=' ', header=None) # 药物相似程度矩阵
-drugs_name = pd.read_table('./Data/DrugsName', sep=' ', header=None).to_numpy()[:, 0]
+drugs_sim = pd.read_table('./Data/DrugSimMat', sep=' ', header=None).to_numpy() # 药物相似程度矩阵
+drugs_name = pd.read_table('./Data/DrugsName', sep=' ', header=None, squeeze=True).to_numpy()
 
-diseases_sim = pd.read_table('./Data/DiseaseSimMat', sep=' ', header=None) # 疾病相似程度矩阵
-diseases_name = pd.read_table('./Data/DiseasesName', sep=' ', header=None).to_numpy()[:, 0]
+diseases_sim = pd.read_table('./Data/DiseaseSimMat', sep=' ', header=None).to_numpy() # 疾病相似程度矩阵
+diseases_name = pd.read_table('./Data/DiseasesName', sep=' ', header=None, squeeze=True).to_numpy()
 
-dd_association = pd.read_table('./Data/DiDrAMat', sep=' ', header=None) # 药物疾病关联矩阵
+dd_association = pd.read_table('./Data/DiDrAMat', sep='\t', header=None).to_numpy() # 药物疾病关联矩阵
+dd_association =  np.delete(dd_association, -1, axis=1)
 dd_association_t = dd_association.transpose()
 
 alpha = 0.3 # 衰减函数参数
