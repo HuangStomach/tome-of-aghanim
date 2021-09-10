701a4c8618d7f900b0d97332c2b5f59eb995c9ea
HuangStomach <nxmbest@qq.com>
2021-09-10 15:44:01 +0800

    mbirw cluster

--- 20210909-MBiRW/lib.py
+++ 20210909-MBiRW/lib.py
@@ -1,43 +1,47 @@
 import numpy as np
 import random
 import math
+import os
 
 '''
 ass_mat 药物和疾病的关联矩阵
 sim_mat 药物或者疾病的相似矩阵
 '''
-def set_par_fun(ass_mat, sim_mat):
-    num = sim_mat.shape[0]
+def set_par_fun(ass_mat, sim_mat_s):
+    sim_mat = sim_mat_s.copy()
+    num = sim_mat.shape[0] # 样本数量
     s_mat = np.zeros(10)
 
-    s = np.zeros(10)
-    n = np.zeros(10)
+    s = np.zeros(10, dtype=int)
+    n = np.zeros(10, dtype=int)
 
+    k = 0
     for i in range(1, num):
-        for j in range(i + 1):
+        for j in range(i):
+            k += 1
             value = int(sim_mat[i][j] * 10)
             sharedValue = np.dot(ass_mat[i], ass_mat[j])
             if value > 9: value = 9
 
             s[value] += 1
-            if sharedValue >= 1: n[value] += 1
+            if sharedValue >= 1: n[value] += 1 # 样本和其他类别样本具有相似的作用样本
 
     for i in range(10):
-        s_mat[i] = s[i] / n[i]
+        s_mat[i] = s[i] / n[i] # 依照样本相似度 转换成样本对儿的百分比
     
-    result_mat = np.zeros((10, 10))
+    random_mat = np.zeros((10, 10))
+    # 使用洗牌法进行打乱
     for _i in range(10):
-        # 使用洗牌法进行打乱
-        s = np.zeros(10)
-        n = np.zeros(10)
+        s = np.zeros(10, dtype=int)
+        n = np.zeros(10, dtype=int)
 
         m = 0
-        rsum = num * (num - 1) / 2 # 所有sim_mat配对可能的数量
-        dmat = np.zeros((1, rsum))
+        rsum = int(num * (num - 1) / 2) # 所有sim_mat配对可能的数量
+        dmat = np.zeros(rsum)
 
         # 打乱之后放回原处
         for i in range(1, num):
-            for j in range(i + 1):
+            for j in range(i):
                 dmat[m] = sim_mat[i][j]
                 m += 1
         
@@ -47,30 +51,58 @@ def set_par_fun(ass_mat, sim_mat):
         
         m = 0
         for i in range(1, num):
-            for j in range(i + 1):
+            for j in range(i):
                 sim_mat[i][j] = dmat[m];
                 m += 1
 
         for i in range(1, num):
-            for j in range(i + 1):
+            for j in range(i):
                 value = int(sim_mat[i][j] * 10)
                 sharedValue = np.dot(ass_mat[i], ass_mat[j])
                 if value > 9: value = 9
 
                 s[value] += 1
-                if sharedValue >= 1: n[value] += 1
+                if sharedValue >= 1: 
+                    n[value] += 1
+        
+        for i in range(10):
+            random_mat[_i][i] = 0.0 if n[i] == 0 else s[i] / n[i]
 
-    for i in range(10):
-        result_mat[_i][i] = s[i] / n[i]
-    r_mat = np.mean(result_mat, axis=1)
+    r_mat = np.mean(random_mat, axis=1) # 进行十次随机化结果，取平均值作为备用对照
     
     threshold = 0;
     for i in range(10):
         result_p = s_mat[i]
-        result_pr = r_mat[i]
+        result_p_random = r_mat[i]
 
-        if result_p >= 1 * result_pr:
-            threshold = i * 0.1;
+        if result_p >= result_p_random:
+            threshold = (i + 1) * 0.1;
             break
 
-    return (math.log(99) - math.log(9999)) / threshold
+    return (math.log(99) - math.log(9999)) / threshold # 得到一个阈值来判断是否有参考价值
+
+def cluster(drugs_sim, diseases_sim, drugs_share, diseases_share, drugs_name, diseases_name):
+    drugs_num = drugs_sim.shape[0]
+    diseases_num = diseases_sim.shape[0]
+
+    file_drug = open('./Output/drugsP.txt', 'w')
+    file_disease = open('./Output/diseasesP.txt', 'w')
+    for i in range(1, drugs_num):
+        for j in range(i):
+            shared_nums = drugs_share[i][j]
+            if shared_nums > 0:
+                content = "{}\t{}\t{:.0f}\n".format(drugs_name[i], drugs_name[j], shared_nums)
+                file_drug.write(content)
+            
+            shared_nums = diseases_share[i][j]
+            if shared_nums > 0:
+                content = "{}\t{}\t{:.0f}\n".format(diseases_name[i], diseases_name[j], shared_nums)
+                file_disease.write(content)
+
+    file_drug.close()
+    file_disease.close()
+
+    os.system('java -jar Lib/cluster_one-1.2.jar Output/drugsP.txt -F csv >> Output/drugs_cluster.csv')
+    os.system('java -jar Lib/cluster_one-1.2.jar Output/diseasesP.txt -F csv >> Output/diseases_cluster.csv')
+            
+    return (1, 2)
--- 20210909-MBiRW/main.py
+++ 20210909-MBiRW/main.py
@@ -11,14 +11,21 @@ diseases_sim = pd.read_table('./Data/DiseaseSimMat', sep=' ', header=None).to_nu
 diseases_name = pd.read_table('./Data/DiseasesName', sep=' ', header=None, squeeze=True).to_numpy()
 
 dd_association = pd.read_table('./Data/DiDrAMat', sep='\t', header=None).to_numpy() # 药物疾病关联矩阵
-dd_association =  np.delete(dd_association, -1, axis=1)
-dd_association_t = dd_association.transpose()
+dd_association =  np.delete(dd_association, -1, axis=1) # 行疾病列药物
+dd_association_t = dd_association.transpose() # 行药物列疾病
 
 alpha = 0.3 # 衰减函数参数
 l = 2
 r = 2
 d = math.log(9999)
 
-drugs_share = np.array(loadmat('./Data/shareWrr.mat')['newWrr'])
-diseases_share = np.array(loadmat('./Data/shareWdd.mat')['newWdd'])
-lib.set_par_fun(dd_association_t, drugs_sim)
+drugs_share = np.array(loadmat('./Data/shareWrr.mat')['newWrr']) # 药物和药物之间共同适应疾病的个数
+diseases_share = np.array(loadmat('./Data/shareWdd.mat')['newWdd']) # 疾病和疾病之间共同适应药物的个数
+
+c_drug = lib.set_par_fun(dd_association_t, drugs_sim)
+c_disease = lib.set_par_fun(dd_association, diseases_sim)
+
+drugs_sim = 1  / (1 + np.exp(c_drug * drugs_sim + d))
+diseases_sim = 1  / (1 + np.exp(c_disease * diseases_sim + d))
+
+(a, b) = lib.cluster(drugs_sim, diseases_sim, drugs_share, diseases_share, drugs_name, diseases_name)
