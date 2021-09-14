787337845a10dacc6f61e89c82f234d345896778
HuangStomach <nxmbest@qq.com>
2021-09-14 15:58:04 +0800

    GRMF 修正之前理解错误的wknkn

--- 20210913-GRMF/lib.py
+++ 20210913-GRMF/lib.py
@@ -8,10 +8,10 @@ class KNKN:
 
     def fit(self, _X) -> P:
         self.X = _X.copy()
-        pass
+        return self
     
     def neighbors(self, i) -> np.ndarray:
-        drugs_sim = self.X.iloc[i].to_numpy()[1:]
-        values = np.sort(drugs_sim)[::-1][1:self._k + 1]
-        indexes = np.argsort(drugs_sim)[::-1][1:self._k + 1]
+        drugs_sim = self.X.iloc[i].to_numpy()
+        values = np.sort(drugs_sim)[::-1][1:self._k+1]
+        indexes = np.argsort(drugs_sim)[::-1][1:self._k+1]
         return (indexes, values)
--- 20210913-GRMF/main.py
+++ 20210913-GRMF/main.py
@@ -1,46 +1,51 @@
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import TruncatedSVD
 from lib import KNKN
 np.set_printoptions(suppress = True)
 
 mod = 'GPCR'
 miu = 0.7
-k = 5
+K = 5
 
 adj_mat = pd.read_table('./Data/adj_{}.txt'.format(mod), sep='\t', header=0, index_col=0).T
 drug_mat = pd.read_table('./Data/drug_{}.txt'.format(mod), sep='\t', header=0, index_col=0)
 target_mat = pd.read_table('./Data/target_{}.txt'.format(mod), sep='\t', header=0, index_col=0)
 (n, m) = adj_mat.shape
 
-Yd = np.zeros(n) # 存储药物K近邻的加权平均值
-Yt = np.zeros(m) # 靶点
+## WKNKN
+Yd = np.zeros((n, m)) # 存储药物K近邻的加权平均值
+Yt = np.zeros((n, m)) # 靶点
 Y = adj_mat.to_numpy(dtype=float)
-knkn = KNKN(k)
+KK = KNKN(K)
+weights = np.zeros(K)
 
-knkn.fit(drug_mat)
+KK.fit(drug_mat)
 for d in range(n):
-    (indexes, values) = knkn.neighbors(d)
-    weights = np.zeros(k)
-    for i in range(k):
-        weights[i] = miu ** (i - 1) * values[i]
-
+    (indexes, values) = KK.neighbors(d)
     z = np.sum(values)
-    adj_neighbors = adj_mat.iloc[indexes].to_numpy()
-    Yd[d] = np.dot(weights, adj_neighbors).sum() / z
+    for i in range(K):
+        weights[i] = (miu ** i) * values[i]
+        Yd[d] += weights[i] * Y[indexes[i]] / z
 
-knkn.fit(target_mat)
+KK.fit(target_mat)
 for t in range(m):
-    (indexes, values) = knkn.neighbors(t)
-    weights = np.zeros(k)
-    for i in range(k):
-        weights[i] = miu ** (i - 1) * values[i]
-
-    z = np.sum(values)
-    adj_neighbors = adj_mat.iloc[:, indexes].to_numpy().transpose()
-    Yt[t] = np.dot(weights, adj_neighbors).sum() / z
+    (indexes, values) = KK.neighbors(t)
+    for i in range(K):
+        weights[i] = (miu ** i) * values[i]
+        Yt[:, t] += weights[i] * Y[:, indexes[i]] / z
 
 for i in range(n):
     for j in range(m):
-        Y[i][j] = max(Y[i][j], (Yd[i] + Yt[j]) / 2)
+        Y[i][j] = max(Y[i][j], (Yd[i][j] + Yt[i][j]) / 2)
 
-print(Y)
+# GRMF
+k = 50
+lambda_l = 0.5
+lambda_d = 0.001
+lambda_t = 0.001
+svd = TruncatedSVD(n_components=k, random_state=42)
+svd.fit(Y)
+Y = svd.transform(Y)
+[u, s, vh] = np.linalg.svd(Y, full_matrices=False)
+print(s)
